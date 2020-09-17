51a1c012579f266d3cc34c2b7e6c487f88b60eaa
COSTCO\fkmohamm <fkmohamm@costco.com>
2020-09-17 13:58:56 -0400

    intial code for single input field

--- src/stories/InputField.stories.js
+++ src/stories/InputField.stories.js
@@ -1,53 +1,82 @@
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 import "../../dist/css/style.css";
-//import { firstNameLabel } from "Label.stories.js";
-
-//var test = import("firstNameLabel");
+import $ from "jquery";
 
 export default{
-    title: 'Input Field',
+    title: 'Single Input Field',
     argTypes: {
-        labelText: { control: 'text' },
-        labelClassName: { control: 'text' },
-        className: {control: 'text'},
+        inputText: { control: 'text' },
       },
 };
 
-// const labelTemplate = ({text, labelClassName}) => {
-//     const label = document.createElement('label');
-//     label.textContent = text;
-//     label.className = labelClassName;
-
-//     return label;
-
-// };
-
-// var firstNameLabel = labelTemplate.bind({});
-// firstNameLabel.args = {
-//        text: 'First Name',
-//        className: 'outline',
-//    };
-
 const inputFieldTemplate = ({ labelText, labelClassName, className }) => {
 
-    const divElement = document.createElement('div');
-    divElement.className=labelClassName;
-    const label = document.createElement('label');
-    label.innerText = labelText;
- //   label.className = labelClassName;
+    const headerElement = document.createElement('h4');
+    headerElement.innerHTML = 'Input Field';
+    //line height is supposed to be 1.1, margin top/bottom should have been 10px, 
+    headerElement.className = 'cds-t4 dark-gray mv2-m fw4 sans-serif'; 
 
-    const inputField = document.createElement('input');
-    inputField.type = 'text';
-    inputField.className = className;
-    inputField.innerHTML=labelText;
+    const parentDivElement = document.createElement("div");
+    const formElement = document.createElement("form");  
+    const formDivElement = document.createElement("div");
+    const outlineDivElement = document.createElement("div");
+    const inputElement = document.createElement("input");
+    const labelElement = document.createElement("label");
 
-    divElement.appendChild(inputField);
-    divElement.appendChild(label);
+    formElement.id = "componentForm";
+    formElement.action = "#";
+    formElement.noValidate = "noValidate";
 
-   // inputField.textContent = firstNameLabel;
-    //inputField.textContent = label;
-    return divElement;
+    formDivElement.className = "form-group input-field-v2";
+
+    outlineDivElement.className = "outerline";   
+
+    inputElement.className = "form-control input-outline";
+    inputElement.id = "firstName";
+    inputElement.name = "firstName";
+    inputElement.label = "First Name";
+/*
+    $(textAreaElement).on('input',function(){
+        character_count('#textarea-character-count', '#textarea-id');
+    });
+
+    $(textAreaElement).on('blur',function(){
+        javasript:void(0);
+    });
+
+    $(inputElement).focusin(function() {
+        $(labelElement).addClass("active");
+    }).focusout(function() {
+        if($(this).val().length === 0) {
+            $(labelElement).removeClass("active");
+            $(this).addClass("error body-copy-error");
+        }
+    });*/
+
+    $(formElement).validate({
+        rules: {
+            firstName: {
+                required: true
+            }
+        },
+        errorClass: "error body-copy-error"
+    });
+ 
+    labelElement.htmlFor = "firstName";
+    //don't have mergin-bottom: 5px, 
+    labelElement.className = "fw4-ns input-field-v2 label-outline";
+    labelElement.innerHTML = "First Name";
+
+    outlineDivElement.appendChild(inputElement);
+    outlineDivElement.appendChild(labelElement);
+
+    formDivElement.appendChild(outlineDivElement);
+
+    formElement.appendChild(formDivElement)
+
+    parentDivElement.appendChild(headerElement);
+    parentDivElement.appendChild(formElement);
+   
+    return parentDivElement;
 
 };
 
