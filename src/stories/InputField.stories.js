2887cb2bd81db53ba4760a7428789bfede3cc562
COSTCO\fkmohamm <fkmohamm@costco.com>
2020-10-10 21:49:22 -0400

    Input field variants

--- src/stories/InputField.stories.js
+++ src/stories/InputField.stories.js
@@ -4,11 +4,23 @@ import $ from "jquery";
 export default{
     title: 'Input Field',
     argTypes: {
-        inputText: { control: 'text' },
-      },
+        variant: {
+            control: {
+                    type:'select',
+                    options: ["Input","Input - On Focus","Input - Completed","Input - Error",
+                            "Input with Help Text","Input with Help Text - On Focus","Input with Help Text - Completed","Input with Help Text - Error",
+                            "Input with Icon","Input with Icon - On Focus","Input with Icon - On Focus","Input with Icon - Completed","Input with Icon - Error",
+                            "Input with Help Link","Input with Help Link - On Focus","Input with Help Link - Complete","Input with Help Link - Error",
+                            "Input with Help Text & Help Link","Input with Help Text & Help Link - On Focus","Input with Help Text & Help Link - Completed","Input with Help Text & Help Link - Error",
+                            "Input with Icon & Help Text","Input with Icon & Help Text - On Focus","Input with Icon & Help Text - Completed","Input with Icon & Help Text - Error",
+                            "Input with Icon & Help Link","Input with Icon & Help Link - On Focus","Input with Icon & Help Link - Completed","Input with Icon & Help Link - Error"
+                            ]
+        }
+      }
+    }
 };
 
-function inputFieldTemplate(inputId,label) {
+function inputFieldTemplate(variant) {
 
     const parentDivElement = document.createElement("div");
     parentDivElement.className = "form-group input-field-v2";
@@ -20,39 +32,121 @@ function inputFieldTemplate(inputId,label) {
     const labelElement = document.createElement("label");
 
     inputElement.className = "form-control input-outline";
-    inputElement.id = inputId;
-    inputElement.name = inputId;
-    inputElement.label = label; 
+    inputElement.type = "text";
+    inputElement.id = "fieldName";
+    inputElement.name = "fieldName";
+    inputElement.label = "Field Name"; 
 
-    labelElement.htmlFor = inputId;
+    labelElement.htmlFor = "fieldName";
     //don't have mergin-bottom: 5px, 
     labelElement.className = "fw4-ns input-field-v2 label-outline";
-    labelElement.innerHTML = label;
+    labelElement.innerHTML = "Field Name";
 
-    const errorSpanElement = document.createElement("span");
-    errorSpanElement.innerText = "";    
+    const spanElement = document.createElement("span");
+    spanElement.innerText = "";  
 
+    
 
-    $(inputElement).on("focusin",function () {
-        $(labelElement).addClass("active");
-        $(errorSpanElement).removeClass("pl2-ns cds-body-copy cds-u-font-bold cds-u-color-text-error");
-        $(errorSpanElement).text("");
+ 
+
+    if(variant === "Input with Help Text" || variant === "Input with Help Text - On Focus" || variant === "Input with Help Text - Completed"
+    || variant === "Input with Icon & Help Text" || variant === "Input with Icon & Help Text - On Focus" || variant === "Input with Icon & Help Text - Completed" 
+            || variant === "Input with Icon & Help Text - Error"){
+        spanElement.innerText = "Help Text"; 
+        $(spanElement).addClass("pl2-ns cds-body-copy cds-u-color-text-light");
+
+    }
+
+    $(inputElement).on("focusin",function(){
+        inputFieldFocusIn();
     }).on("focusout",function () {
-        if ($(this).val().length === 0) {
+        inputFieldFocusOut();
+    });
+
+    function inputFieldFocusIn() {
+        $(labelElement).removeClass("error");
+        $(inputElement).removeClass("error");
+        $(labelElement).addClass("active");
+        $(spanElement).removeClass("pl2-ns cds-body-copy cds-u-font-bold cds-u-color-text-error");
+        $(spanElement).text("");
+    }
+
+    function inputFieldFocusOut() {
+        if ($(inputElement).val().length === 0) {
             $(labelElement).addClass("error");
             $(inputElement).addClass("error");
-            $(errorSpanElement).text("Error Message")
-            $(errorSpanElement).addClass("pl2-ns cds-body-copy cds-u-font-bold cds-u-color-text-error");
+            $(spanElement).text("Error Message")
+            $(spanElement).addClass("pl2-ns cds-body-copy cds-u-font-bold cds-u-color-text-error");
            } else{
             $(labelElement).removeClass("error");
-            $(this).removeClass("error");
+            $(inputElement).removeClass("error");
             $(labelElement).addClass("active");
            }
-    });
+    }
 
     outlineDivElement.appendChild(inputElement);
     outlineDivElement.appendChild(labelElement);
-    outlineDivElement.appendChild(errorSpanElement);
+    if(variant === "Input with Icon" || variant === "Input with Icon - On Focus" || variant === "Input with Icon - Completed" || variant === "Input with Icon - Error"
+        || variant === "Input with Icon & Help Text" || variant === "Input with Icon & Help Text - On Focus" || variant === "Input with Icon & Help Text - Completed" || variant === "Input with Icon & Help Text - Error"
+        || variant === "Input with Icon & Help Link" || variant === "Input with Icon & Help Link - On Focus" || variant === "Input with Icon & Help Link - Completed" || variant === "Input with Icon & Help Link - Error"){
+        if(variant === "Input with Icon - Error" || variant === "Input with Icon & Help Text - Error" || variant === "Input with Icon & Help Link - Error"){
+            spanElement.innerText = "Error Message";
+            $(spanElement).addClass("pl2-ns cds-body-copy cds-u-font-bold cds-u-color-text-error");
+        }
+        
+        const iconElement = document.createElement("div");
+        iconElement.className = "h1-ns w1-ns get-help fl-ns";
+        $(inputElement).addClass("w-90-ns fl-ns");
+        outlineDivElement.appendChild(iconElement);
+
+        
+
+         
+
+    }
+    outlineDivElement.appendChild(spanElement);
+
+    if(variant === "Input with Help Text & Help Link" || variant === "Input with Help Text & Help Link - On Focus" 
+            || variant === "Input with Help Text & Help Link - Completed" || variant === "Input with Help Text & Help Link - Error"){
+        if(variant === "Input with Help Text & Help Link - Error"){
+            spanElement.innerText = "Error Message";
+            $(spanElement).addClass("pl2-ns cds-body-copy cds-u-font-bold cds-u-color-text-error");
+        }else{
+            spanElement.innerText = "Help Text";
+            $(spanElement).addClass("pl2-ns cds-body-copy cds-u-color-text-light");
+        } 
+        
+
+        const anchorElement = document.createElement("a");
+        anchorElement.href = "url";
+        anchorElement.innerText = "Help Link"; 
+        $(anchorElement).addClass("pl2-ns cds-body-copy cds-u-color-text-link");
+
+        const newLineElement = document.createElement("br");
+        outlineDivElement.appendChild(newLineElement);
+
+        outlineDivElement.appendChild(anchorElement);
+
+    }else if(variant === "Input with Help Link" || variant === "Input with Help Link - On Focus" 
+            || variant === "Input with Help Link - Complete" || variant === "Input with Help Link - Error"
+            || variant === "Input with Icon & Help Link" || variant === "Input with Icon & Help Link - On Focus" || variant === "Input with Icon & Help Link - Completed" || variant === "Input with Icon & Help Link - Error"){
+
+        if(variant === "Input with Help Link - Error"){
+            spanElement.innerText = "Error Message";
+            $(spanElement).addClass("pl2-ns cds-body-copy cds-u-font-bold cds-u-color-text-error");
+
+            const newLineElement = document.createElement("br");
+            outlineDivElement.appendChild(newLineElement);
+
+        }
+        const anchorElement = document.createElement("a");
+        anchorElement.href = "url";
+        anchorElement.innerText = "Help Link"; 
+        $(anchorElement).addClass("pl2-ns cds-body-copy cds-u-color-text-link");
+
+        outlineDivElement.appendChild(anchorElement);
+    }
+
 
     parentDivElement.appendChild(outlineDivElement);
 
@@ -61,7 +155,7 @@ function inputFieldTemplate(inputId,label) {
 };
 
  
-function formTemplate({ labelText, labelClassName, className }) {
+function formTemplate({ variant }) {
 
     const headerElement = document.createElement('h4');
     headerElement.innerHTML = 'Input Field';
@@ -75,7 +169,7 @@ function formTemplate({ labelText, labelClassName, className }) {
     formElement.action = "#";
     formElement.noValidate = "noValidate";
 
-    formElement.appendChild(inputFieldTemplate('fieldName','Field Name'));
+    formElement.appendChild(inputFieldTemplate(variant));
 
     parentDivElement.appendChild(headerElement);
     parentDivElement.appendChild(formElement);
@@ -84,10 +178,28 @@ function formTemplate({ labelText, labelClassName, className }) {
  
 }
 
-export const FieldName = formTemplate.bind({});
-    FieldName.args = {
-        labelText: 'Field Name',
-        labelClassName: 'label-outline form-label',
-        className: 'input-outline form-control',
-    };
+export const InputField = formTemplate.bind({});
+InputField.args = {
+    variant: '',
+};
+
+export const InputFieldIcon = formTemplate.bind({});
+InputFieldIcon.args = {
+    variant: 'Input with Icon',
+};
+
+export const InputFieldHelpText = formTemplate.bind({});
+InputFieldHelpText.args = {
+    variant: 'Input with Help Text',
+};
+
+export const InputFieldHelpLink = formTemplate.bind({});
+InputFieldHelpLink.args = {
+    variant: 'Input with Help Link',
+};
+
+export const InputFieldHelpTextLink = formTemplate.bind({});
+InputFieldHelpTextLink.args = {
+    variant: 'Input with Help Text & Help Link',
+};
 
