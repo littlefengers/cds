96af2d0cb377d7d55888f2cda0c007e42e4f05e9
Feng Tien <feng.tien@gmail.com>
2020-11-05 09:55:29 -0800

    Switching back to args

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,12 +1,13 @@
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withDsm } from '@invisionapp/dsm-storybook';
 import "../../dist/css/style.css";
 
 export default {
   title: 'Button',
   argTypes: {
     label: { control: 'text' },
-  },
+  }
 };
 
 const Template = ({ onClick, label, className }) => {
@@ -17,6 +18,30 @@ const Template = ({ onClick, label, className }) => {
   btn.addEventListener('click', onClick);
   return btn;
 };
+
+
+// export const Primary = ( label ) => {
+//   const btn = document.createElement('button');
+//   btn.type = 'button';
+//   btn.innerText = label
+//   btn.className = "cds-btn cds-btn--primary"
+//   btn.addEventListener('click', action('onClick'));
+//   return btn;
+// };
+
+
+// Primary.args = {
+//   label: 'hello world'
+// }
+
+
+// Primary.story = {
+//   decorators: [withDsm],
+//   parameters: {
+//     'in-dsm': { id: '5e8230abdf84e2a27d2c579c' }
+//   }
+// }
+
 
 export const Primary = Template.bind({});
 Primary.args = {
@@ -46,42 +71,6 @@ PrimaryAlternate.args = {
  className: 'cds-btn cds-btn--primary-alternate',
 };
 
-// const Template = ({ onClick, children }) => {
-//   const btn = document.createElement('button');
-//   btn.type = 'button';
-//   btn.innerText = children;
-//   btn.addEventListener('click', onClick);
-//   return btn;
-// };
 
-export const Text = Template.bind({});
-Text.args = {
-  children: 'Button',
-  onClick: action('onClick'),
-};
 
-export const Emoji = Template.bind({});
-Emoji.args = {
-  children: '😀 😎 👍 💯',
-};
-
-export const TextWithAction = () => {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = 'Trigger Action';
-  btn.addEventListener('click', () => action('This was clicked')());
-  return btn;
-};
-
-TextWithAction.storyName = 'With an action';
-TextWithAction.parameters = { notes: 'My notes on a button with emojis' };
-
-export const ButtonWithLinkToAnotherStory = () => {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = 'Go to Welcome Story';
-  btn.addEventListener('click', linkTo('example-introduction--page'));
-  return btn;
-};
-
-ButtonWithLinkToAnotherStory.storyName = 'button with link to another story';
+PrimaryAlternate.storyName = 'button with link to another story';
